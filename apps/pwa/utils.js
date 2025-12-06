c02a2c13ec9869f569adf68e372ad24838157d03
Your Name <your.email@example.com>
2025-12-06 09:19:35 +0530

    Fix calculation bug

--- apps/pwa/utils.js
+++ apps/pwa/utils.js
@@ -1,7 +1,7 @@
 // Simple utility functions for Kanna
 
 function calculateLeadFee(leads, ratePerLead = 25) {
-  return leads * ratePerLead +1;
+  return leads * ratePerLead;
 }
 
 function isValidPhone(phone) {
